70bbae5e476c080770cc97eb419df2fa44624272
gayana06 <gayanachandrasekara@gmail.com>
2013-11-03 01:52:23 +0000

    Test Codes

---
+++ TestPCCode/Bluetooth Tutorial/Bluetooth Tutorial/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Bluetooth_Tutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbServer = new System.Windows.Forms.RadioButton();
+            this.rbClient = new System.Windows.Forms.RadioButton();
+            this.bGo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(10, 5);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(635, 240);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(5, 360);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(640, 20);
+            this.tbText.TabIndex = 1;
+            this.tbText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbText_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(655, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 134);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbServer);
+            this.groupBox1.Controls.Add(this.rbClient);
+            this.groupBox1.Location = new System.Drawing.Point(655, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Type";
+            // 
+            // rbServer
+            // 
+            this.rbServer.AutoSize = true;
+            this.rbServer.Checked = true;
+            this.rbServer.Location = new System.Drawing.Point(5, 40);
+            this.rbServer.Name = "rbServer";
+            this.rbServer.Size = new System.Drawing.Size(56, 17);
+            this.rbServer.TabIndex = 1;
+            this.rbServer.TabStop = true;
+            this.rbServer.Text = "Server";
+            this.rbServer.UseVisualStyleBackColor = true;
+            // 
+            // rbClient
+            // 
+            this.rbClient.AutoSize = true;
+            this.rbClient.Location = new System.Drawing.Point(5, 20);
+            this.rbClient.Name = "rbClient";
+            this.rbClient.Size = new System.Drawing.Size(51, 17);
+            this.rbClient.TabIndex = 0;
+            this.rbClient.Text = "Client";
+            this.rbClient.UseVisualStyleBackColor = true;
+            // 
+            // bGo
+            // 
+            this.bGo.Location = new System.Drawing.Point(815, 255);
+            this.bGo.Name = "bGo";
+            this.bGo.Size = new System.Drawing.Size(75, 23);
+            this.bGo.TabIndex = 4;
+            this.bGo.Text = "Go!!!!";
+            this.bGo.UseVisualStyleBackColor = true;
+            this.bGo.Click += new System.EventHandler(this.bGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 388);
+            this.Controls.Add(this.bGo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.tbOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbServer;
+        private System.Windows.Forms.RadioButton rbClient;
+        private System.Windows.Forms.Button bGo;
+    }
+}
+
---
+++ TestPCCode/Bluetooth Tutorial/Bluetooth Tutorial/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using InTheHand;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+using System.IO;
+
+
+
+namespace Bluetooth_Tutorial
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> items;
+        public Form1()
+        {
+            items = new List<string>();
+            InitializeComponent();
+        }
+
+        private void bGo_Click(object sender, EventArgs e)
+        {
+            if (serverStarted)
+            {
+                updateUI("Server already started silly sausage!");
+                return;
+            }
+            if (rbClient.Checked)
+            {
+                startScan();
+            }
+            else
+            {
+                connectAsServer();
+            }
+        }
+
+
+        private void startScan()
+        {
+            listBox1.DataSource = null;
+            listBox1.Items.Clear();
+            items.Clear();
+            Thread bluetoothScanThread = new Thread(new ThreadStart(scan));
+            bluetoothScanThread.Start();
+        }
+        BluetoothDeviceInfo[] devices;
+        private void scan()
+        {
+
+            updateUI("Starting Scan..");
+            BluetoothClient client = new BluetoothClient();
+            devices = client.DiscoverDevicesInRange();
+            updateUI("Scan complete");
+            updateUI(devices.Length.ToString()+" devices discovered");
+            foreach (BluetoothDeviceInfo d in devices)
+            {
+                items.Add(d.DeviceName);
+            }
+
+            updateDeviceList();
+        }
+
+        private void connectAsServer()
+        {
+            Thread bluetoothServerThread = new Thread(new ThreadStart(ServerConnectThread));
+            bluetoothServerThread.Start();
+        }
+
+        private void connectAsClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        Guid mUUID = new Guid("8ce255c0-200a-11e0-ac64-0800200c9a66");
+        bool serverStarted = false;
+        public void ServerConnectThread()
+        {
+            serverStarted = true;
+            updateUI("Server started, waiting for clients");
+            BluetoothListener blueListener = new BluetoothListener(mUUID);
+            blueListener.Start();
+            BluetoothClient conn = blueListener.AcceptBluetoothClient();
+            updateUI("Client has connected");
+
+            Stream mStream = conn.GetStream();
+            while (true)
+            {
+                try
+                {
+                    //handle server connection
+                    byte[] received = new byte[1024];
+                    mStream.Read(received, 0, received.Length);
+                    updateUI("Received: " + Encoding.ASCII.GetString(received));
+                    byte[] sent = Encoding.ASCII.GetBytes("Hello World");
+                    mStream.Write(sent, 0, sent.Length);
+                }
+                catch (IOException exception)
+                {
+                    updateUI("Client has disconnected!!!!");
+                }
+            }
+
+        }
+
+        private void updateUI(string message)
+        {
+            Func<int> del = delegate()
+            {
+                tbOutput.AppendText(message + System.Environment.NewLine);
+                return 0;
+            };
+            Invoke(del);
+        }
+
+        private void updateDeviceList()
+        {
+            Func<int> del = delegate()
+            {
+                listBox1.DataSource = items;
+                return 0;
+            };
+            Invoke(del);
+        }
+
+
+        BluetoothDeviceInfo deviceInfo;
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            deviceInfo = devices.ElementAt(listBox1.SelectedIndex);
+            updateUI(deviceInfo.DeviceName + " was selected, attempting connect");
+
+            if (pairDevice())
+            {
+                updateUI("device paired..");
+                updateUI("starting connect thread");
+                Thread bluetoothClientThread = new Thread(new ThreadStart(ClientConnectThread));
+                bluetoothClientThread.Start();
+
+            }
+            else
+            {
+                updateUI("Pair failed");
+            }
+
+        }
+
+        private void ClientConnectThread()
+        {
+            BluetoothClient client = new BluetoothClient();
+            updateUI("attempting connect");
+            client.BeginConnect(deviceInfo.DeviceAddress, mUUID, this.BluetoothClientConnectCallback, client);
+
+        }
+
+        void BluetoothClientConnectCallback(IAsyncResult result)
+        {
+            BluetoothClient client = (BluetoothClient)result.AsyncState;
+            client.EndConnect(result);
+
+            Stream stream = client.GetStream();
+            stream.ReadTimeout = 1000;
+            byte[] buf=new byte[1024];
+            while (true)
+            {
+                while (!ready) ;
+                stream.Write(message, 0, message.Length);
+                stream.Read(buf, 0, buf.Length);
+                string val = System.Text.Encoding.UTF8.GetString(buf);
+                updateUI(val);
+                ready = false;
+            }
+            
+
+        }
+
+        string myPin = "1234";
+        private bool pairDevice()
+        {
+            if (!deviceInfo.Authenticated)
+            {
+                if (!BluetoothSecurity.PairRequest(deviceInfo.DeviceAddress, myPin))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        bool ready = false;
+        byte[] message;
+        private void tbText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                message = Encoding.ASCII.GetBytes(tbText.Text);
+                ready = true;
+                tbText.Clear();
+            }
+        }
+
+        
+    }
+}
